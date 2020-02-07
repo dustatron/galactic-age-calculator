b5ec76e16ae3bb744d0ce0c5ca08dc2a5fda4cdd
Dusty <dustymccord@gmail.com>
2020-02-07 09:38:20 -0800

    update contrcutor for AgeCalc to include day, month, year.

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -1,9 +1,11 @@
 import { AgeCalc } from ".././src/planet-age.js";
 
 describe("AgeCalc Object", () => {
-  let ageCalc = new AgeCalc(24, 80, "Mars");
+  let ageCalc = new AgeCalc(14, 2, 1982, 80, "Mars");
   let defaultObj = {
-    age: 24,
+    birthDay: 14,
+    birthMonth: 2,
+    birthYear: 1982,
     totalLife: 80,
     planet: "Mars"
   };
--- src/planet-age.js
+++ src/planet-age.js
@@ -1,7 +1,11 @@
 export class AgeCalc {
-  constructor(age, life, planet) {
-    this.age = age;
+  constructor(day, month, year, life, planet) {
+    this.birthDay = day;
+    this.birthMonth = month;
+    this.birthYear = year;
     this.totalLife = life;
     this.planet = planet;
   }
-}
+
+  getTotalDays() {}
+} //end class
