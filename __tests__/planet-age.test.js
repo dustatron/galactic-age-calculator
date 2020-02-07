a4685b31e627f73a6873854919659130389b6e70
Dusty <dustymccord@gmail.com>
2020-02-07 11:19:15 -0800

    add passing test for getAllAges.

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -38,15 +38,15 @@ describe("AgeCalc Object", () => {
 
   describe("getAllAges", () => {
     let ages = {
-      mercury: 157,
-      venus: 61,
-      earth: 38,
-      mars: 20,
-      jupiter: 3,
-      saturn: 1,
-      uranus: 0,
-      neptune: 0,
-      pluto: 0
+      mercury: "157.53",
+      venus: "61.70",
+      earth: "37.96",
+      mars: "20.18",
+      jupiter: "3.20",
+      saturn: "1.29",
+      uranus: "0.45",
+      neptune: "0.23",
+      pluto: "0.15"
     };
     test("should return object with users age for each planet", () => {
       expect(ageCalc.getAllAges()).toEqual(ages);
--- src/planet-age.js
+++ src/planet-age.js
@@ -12,6 +12,20 @@ export class AgeCalc {
     return [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
   }
 
+  planetRotations() {
+    return {
+      mercury: 88.0,
+      venus: 224.7,
+      earth: 365.2,
+      mars: 687.0,
+      jupiter: 4331,
+      saturn: 10747,
+      uranus: 30589,
+      neptune: 59800,
+      pluto: 90560
+    };
+  }
+
   getThisYearsDays() {
     let monthTotals = this.monthsArr();
     let date = new Date();
@@ -45,6 +59,28 @@ export class AgeCalc {
   }
 
   getAllAges() {
-    //return age on this planet.
+    let orbits = this.planetRotations();
+    let usersDays = this.getTotalDays();
+    let orbitsArr = Object.entries(orbits);
+    let ages = {
+      mercury: 0,
+      venus: 0,
+      earth: 0,
+      mars: 0,
+      jupiter: 0,
+      saturn: 0,
+      uranus: 0,
+      neptune: 0,
+      pluto: 0
+    };
+
+    for (var planet in orbits) {
+      ages[planet] = (usersDays / orbits[planet]).toFixed(2);
+    }
+
+    // let ages = orbitsArr.map((item) => {
+    //   return [ item[0], (usersDays / item[1]).toFixed(2) ];
+    // });
+    return ages;
   }
 } //end class
